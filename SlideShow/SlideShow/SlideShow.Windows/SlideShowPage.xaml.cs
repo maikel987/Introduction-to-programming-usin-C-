6653d030542f087f3adf695c4cb5a2891bcf5ecb
Maikel SITBON <maikelsitbon@gmail.com>
2018-11-06 18:52:30 +0200

    Add files via upload

---
+++ SlideShow/SlideShow/SlideShow.Windows/SlideShowPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SlideShow
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SlideShowPage : Page
+    {
+        // C'tor
+        public SlideShowPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnSelectPictureHandler(object sender, PointerRoutedEventArgs e)
+        {
+            Image selectedImage = (Image)sender;
+            imgMainImage.Source = selectedImage.Source;
+
+        }
+
+    }
+}
