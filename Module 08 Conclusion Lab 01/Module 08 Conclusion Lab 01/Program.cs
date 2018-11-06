7cc882e1c03748a4acb1ff7f3f00fc48c1e72070
Maikel SITBON <maikelsitbon@gmail.com>
2018-11-06 17:30:48 +0200

    Add files via upload

---
+++ Module 08 Conclusion Lab 01/Module 08 Conclusion Lab 01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_08_Conclusion_Lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int counter = 0;
+
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                while (num%2 ==0)
+                {
+                    counter++;
+                    num = num / 2;
+                }
+
+
+            }else
+            {
+                Console.WriteLine("wrong number");
+            }
+            Console.WriteLine("this number is divisible " + counter + " time.");
+            Console.ReadLine();
+        }
+    }
+}
